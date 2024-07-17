5089e8816d4ff0ca50f4d5903a40c9e1d1bc93d4
Aphinya09 <Aphinya091247@gmail.com>
2024-07-17 19:32:38 +0700

    change to thai

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,16 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ-สกุล *"
+        hint="ชื่อเเละนามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
       <q-input
         filled
         type="number"
-        v-model="age"
-        label="Your age *"
+        v-model="อายุเท่าไหร่"
+        label="อายุ *"
         lazy-rules
         :rules="[
           val => val !== null && val !== '' || 'Please type your age',
